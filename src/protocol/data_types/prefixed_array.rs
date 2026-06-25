cb56e24d890205ddeb4172542ae45e1845c88b5b
TheBlckbird <theblckbrd@proton.me>
2026-06-25 18:50:22 +0200

    Improve arrays

--- src/protocol/data_types/prefixed_array.rs
+++ src/protocol/data_types/prefixed_array.rs
@@ -8,51 +8,47 @@ use crate::protocol::{
     },
 };
 
-impl<T> Serialize for &[T]
-where
-    T: Serialize,
-{
-    fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), ser::Error> {
-        VarInt(self.len() as i32).serialize(writer)?;
+macro_rules! impl_prefixed_array_ser_de {
+    ($({$type:ty, $transform:expr}),*$(,)?) => {
+        $(
+            impl<T> Serialize for $type
+            where
+                T: Serialize,
+            {
+                fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), ser::Error> {
+                    VarInt(self.len() as i32).serialize(writer)?;
 
-        for element in self.iter() {
-            element.serialize(writer)?;
-        }
+                    for element in self.iter() {
+                        element.serialize(writer)?;
+                    }
 
-        Ok(())
-    }
+                    Ok(())
+                }
+            }
+
+            impl<T> Deserialize for $type
+            where
+                T: Deserialize,
+            {
+                fn deserialize<R: BufRead>(reader: &mut R) -> Result<Self, de::Error> {
+                    let length = *VarInt::deserialize(reader)? as usize;
+                    let mut result = Vec::with_capacity(length);
+
+                    for _ in 0..length {
+                        result.push(T::deserialize(reader)?);
+                    }
+
+                    Ok($transform(result))
+                }
+            }
+        )*
+    };
 }
 
-impl<T> Serialize for Box<[T]>
-where
-    T: Serialize,
-{
-    fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), ser::Error> {
-        VarInt(self.len() as i32).serialize(writer)?;
-
-        for element in self.iter() {
-            element.serialize(writer)?;
-        }
-
-        Ok(())
-    }
-}
-
-impl<T> Deserialize for Box<[T]>
-where
-    T: Deserialize,
-{
-    fn deserialize<R: BufRead>(reader: &mut R) -> Result<Self, de::Error> {
-        let length = *VarInt::deserialize(reader)? as usize;
-        let mut result = Vec::with_capacity(length);
-
-        for _ in 0..length {
-            result.push(T::deserialize(reader)?);
-        }
-
-        Ok(result.into_boxed_slice())
-    }
-}
+impl_prefixed_array_ser_de!(
+    {Box<[T]>, |result: Vec<T>| result.into_boxed_slice()},
+    {Vec<T>, |result: Vec<T>| result},
+);
 
 #[cfg(test)]
 mod tests {
@@ -62,7 +58,7 @@ mod tests {
 
     #[test]
     fn test_it_works() {
-        let input = [1u8, 2, 3, 4, 5].as_slice();
+        let input = vec![1u8, 2, 3, 4, 5];
         let mut writer = Vec::new();
         input.serialize(&mut writer).unwrap();
 
