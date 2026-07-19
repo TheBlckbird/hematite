4cd0dbcb55ce73c2aef2b319212ae862957f929e
TheBlckbird <theblckbrd@proton.me>
2026-07-19 22:54:40 +0200

    Merge position core type and data type

--- src/protocol/data_types/position.rs
+++ src/protocol/data_types/position.rs
@@ -1,41 +1,12 @@
 use std::io::{BufRead, Write};
 
-use crate::protocol::ser_de::{
-    de::{self, Deserialize},
-    ser::{self, Serialize},
+use crate::{
+    core::position::Position,
+    protocol::ser_de::{
+        de::{self, Deserialize},
+        ser::{self, Serialize},
+    },
 };
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Position {
-    pub x: i32,
-    pub y: i16,
-    pub z: i32,
-}
-
-impl Position {
-    const I26_MIN: i32 = -33_554_432;
-    const I26_MAX: i32 = 33_554_431;
-    const I12_MIN: i16 = -2048;
-    const I12_MAX: i16 = 2047;
-
-    /// Creates a new position from x, y and z.
-    ///
-    /// ## Panics
-    ///
-    /// Panics if x or z are longer than 26 bytes or y is longer than 12 bytes
-    pub fn new(x: i32, y: i16, z: i32) -> Self {
-        assert!(x >= Self::I26_MIN);
-        assert!(x <= Self::I26_MAX);
-
-        assert!(y >= Self::I12_MIN);
-        assert!(y <= Self::I12_MAX);
-
-        assert!(z >= Self::I26_MIN);
-        assert!(z <= Self::I26_MAX);
-
-        Self { x, y, z }
-    }
-}
 
 impl Serialize for Position {
     fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), ser::Error> {
